cdd9788e0bc368487f16a4f98ba2d48e4efcde98
steve.sidelar <steve.sidelar@740322e4-be33-11de-afc4-699a8c260df9>
2010-02-11 22:42:27 +0000

    SourceHeader first real world usage - headers of source header have been edited using SourceHeader...

--- trunk/src/SourceHeader.Gui/src/sourceheader/gui/util/tree/FilesTreeCellRenderer.java
+++ trunk/src/SourceHeader.Gui/src/sourceheader/gui/util/tree/FilesTreeCellRenderer.java
@@ -1,6 +1,14 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * file FilesTreeCellRenderer.java
+ *
+ * This file is part of SourceHeader project.
+ *
+ * SourceHeader is software for easier maintaining source files' headers.
+ * project web: http://code.google.com/p/source-header/
+ * author: Steve Sindelar
+ * licence: New BSD Licence
+ * 
+ * (c) Steve Sindelar
  */
 
 package sourceheader.gui.util.tree;
