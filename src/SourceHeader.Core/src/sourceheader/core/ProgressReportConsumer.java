465cc75d2b4399ba68e5a9c6f538c937f06de4c4
steve.sidelar <steve.sidelar@740322e4-be33-11de-afc4-699a8c260df9>
2010-02-11 22:42:27 +0000

    SourceHeader first real world usage - headers of source header have been edited using SourceHeader...

--- src/SourceHeader.Core/src/sourceheader/core/ProgressReportConsumer.java
+++ src/SourceHeader.Core/src/sourceheader/core/ProgressReportConsumer.java
@@ -1,3 +1,16 @@
+/*
+ * file FilesTreeFactory.java
+ *
+ * This file is part of SourceHeader project.
+ *
+ * SourceHeader is software for easier maintaining source files headers.
+ * project web: http://code.google.com/p/source-header/
+ * author: Steve Sindelar
+ * licence: New BSD Licence
+ *
+ * (c) Steve Sindelar
+ */
+
 package sourceheader.core;
 
 /**
